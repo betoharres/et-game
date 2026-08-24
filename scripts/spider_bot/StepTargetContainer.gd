1de0f80bb753cd7efe1035e6e33e1873e5b55e95
QuRahn <105900975+QuRahn@users.noreply.github.com>
2026-08-24 18:55:56 -0300

    IK da bot com previsão de rotação; mudança final de hoje do teteu

--- scripts/spider_bot/StepTargetContainer.gd
+++ scripts/spider_bot/StepTargetContainer.gd
@@ -1,12 +1,23 @@
 extends Node3D
 
 @onready var parent: Node3D = get_parent_node_3d()
+var rotation_motion: float = 0.0
 
 func _physics_process(_delta: float) -> void:
+	
+	
 	var linear_motion: Vector3 = parent.get("current_velocity") as Vector3
 	linear_motion.y = 0.0
 
 	var angular_motion: float = float(parent.get("current_rotation_velocity"))
+
+	var target_rotation_motion : float = angular_motion * 1.3
+
+	rotation_motion = lerp(
+		rotation_motion,
+		target_rotation_motion,
+		0.25
+	)
 
 	var target_basis: Basis = (
 		Basis(Vector3.UP, angular_motion) * parent.global_transform.basis
