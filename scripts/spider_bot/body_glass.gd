f69b40212acfcf07705474ccfd748d4c07e4c5f6
QuRahn <105900975+QuRahn@users.noreply.github.com>
2026-08-25 20:31:39 -0300

    camada de visão preparada para sistema+complexo

---
+++ scripts/spider_bot/body_glass.gd
@@ -0,0 +1,25 @@
+extends MeshInstance3D
+
+@export var cycle_time : float = 10.0
+
+var effect_mode : float = 0.0
+var timer : float = 0.0
+
+@onready var mat: ShaderMaterial = material_override
+
+
+func _process(delta: float) -> void:
+	timer += delta
+
+	if timer >= cycle_time:
+		timer -= cycle_time
+
+		effect_mode += 1.0
+
+		if effect_mode >= 5.0:
+			effect_mode = 0.0
+
+		mat.set_shader_parameter(
+			"effect_mode",
+			effect_mode
+		)
